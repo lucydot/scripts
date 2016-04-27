be82e6a80f8e5f20823b0e4f8f63e32f27db881c
Lucy <l.whalley@bath.ac.uk>
2016-04-27 17:49:10 +0100

    spacetools with calculate direction function added

--- bitsbobs/spacetools.py
+++ bitsbobs/spacetools.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
 
 """ 
-Tools to help moving between real and reciprocal space. Calculated values
-are stored in a dictionary. Units of length are angstroms (in real space) 
-and 1/angstroms (in reciprocal space).
+Functions to help moving between real and reciprocal space. When runnning
+in main the calculated values are stored in a dictionary. Units of length 
+are angstroms (in real space) and 1/angstroms (in reciprocal space).
 """
-
+  
 import numpy
+import numpy.ma
 import argparse
 
 def vector_length(vector):
@@ -70,19 +71,22 @@ def calculate_distance(a_star_length,b_star_length,c_star_length,
     values['distance_to_hsp'] = vector_length(vector)
     return values
 
-def calculate_number_kpoints(sampling_density, distance):
+def calculate_number_kpoints(sampling_distance, distance):
     """ Takes k point density and distance in reciprocal space.
     Returns the number of k points required to sample gamma to 
     the high symmetry point at this density.
     """
-    values['number_kpoints'] = (1/sampling_density)*distance
+    values['number_kpoints'] = (1/sampling_distance)*distance+1
     return values
+
+    # +- 1 because ten k-point sections of equal length will need
+    # 11 k points (assuming start and end with a k-point) 
 
 def calculate_sampling_distance(number_kpoints, distance): 
     """Takes number of k points and distance in reciprocal space.
     Returns the distance between each k-point.
     """
-    values['sampling_distance'] = distance / number_kpoints
+    values['sampling_distance'] = distance / (number_kpoints-1)
     return values    
 
 
@@ -92,26 +96,35 @@ def print_to_screen(values):
       
     for key, value in sorted(values.items()):
         if type(value) == numpy.ndarray:
-            value = array_to_list(value)
+            value = str(array_to_list(value))
         if type(value) == float:
-            value = round(value, 7)
-        print (key, "=", value)
+            value = str(round(value, 7))
+        if type(value) == list:
+            value = str(value)
+        print (key + " = " + value)
 
 def calculate_direction(a, b):
-    """ Takes two 1x3 numpy arrays. Returns the direction between
-    them.
+    """ Takes two k points as 1x3 lists ([a,b,c],[a,b,c]). Returns 
+    the direction between them.
     """
+    difference = numpy.subtract(a,b)
+    if numpy.count_nonzero(difference) < 1:
+        print ("The two k-points are equal")
+        return numpy.array([0,0,0])  
+  
     # we need to find the smallest none-zero value within a-b
     # return array with invalid entries where values are equal
-    direction_masked = np.ma.masked_equal(a - b, 0)
+    a = numpy.array(a)
+    b = numpy.array(b)
+    direction_masked = numpy.ma.masked_equal(a - b, 0)
     # fill invalid elements of array with a large number s
-    direction_filled = np.ma.filled(direction_masked, 10**6)
+    direction_filled = numpy.ma.filled(direction_masked, 10**6)
     # return absolute values of each element 
-    direction_absolute = np.absolute(direction_filled)
-    smallest = np.amin(direction_absolute)
-
+    direction_absolute = numpy.absolute(direction_filled)
+    smallest = numpy.amin(direction_absolute)
+    print (smallest)
     # use the minimum absolute value as a divisor a-b
-    direction = a - b / smallest
+    direction = (b - a) / smallest
     return direction
 
 if __name__ == "__main__":
@@ -120,22 +133,23 @@ if __name__ == "__main__":
     parser.add_argument( '--space', '-s', type=str, required=True,
                         help="""<required> space of input vector""", 
                         choices=['real', 'reciprocal'])
-    parser.add_argument('--vectors', '-v', type=float, required=True,
-                        help="""<required> lattice vectors""",
-                        metavar='a1 a2 a3 b1 b2 b3 c1 c2 c3'
+    parser.add_argument('--vectors', '-v', type=str, required=True,
+                        help="""<required> input lattice vectors""",
+                        nargs=3,
+                        metavar='abc'
                         )
     parser.add_argument('-p', '--high_symmetry_point', type=float, 
-                        metavar='0 0.5 0',
+                        metavar='0.5',
                         help="""<optional> position of high symmetry point
                         in fractional coordinates of k-space""",
-                        required=False, default=None)
+                        nargs = 3, required=False, default=None)
     parser.add_argument('-d', '--sampling_distance', action='store', type=float,
                         help="""<optional> the k point sampling distance
                         in units 1/Angstrom """, metavar='0.01', 
                         required=False, default=None)
     parser.add_argument('-n', '--number_kpoints', type=int, metavar='50',
                        help="""<optional> the number of k points between 
-                       gamma and high_symmetry_point """,
+                       gamma and high_symmetry_point inclusive""",
                        required=False, default=None)
     parser.add_argument('-m', '--scaling', type=float, metavar='5.235',
                         help="""<optional> scales input lattice vectors 
@@ -143,13 +157,12 @@ if __name__ == "__main__":
                         amount (defalt: 1)""", required=False, 
                         default=1)
     args = parser.parse_args()
-    
-    vectors = numpy.array(args.vectors)
+   
     high_symmetry_point = numpy.array(args.high_symmetry_point)
     
-    a_in = vectors[:3]
-    b_in = vectors[3:6]
-    c_in = vectors[6:9]
+    a_in = [int(x) for x in args.vectors[0]]
+    b_in = [int(x) for x in args.vectors[1]]
+    c_in = [int(x) for x in args.vectors[2]]
     
     values = transform_space(a_in, b_in, c_in, 
                     args.space, scaling=args.scaling)
@@ -160,9 +173,9 @@ if __name__ == "__main__":
                            values['c_star_length'],
                            high_symmetry_point)
         if args.sampling_distance is not None:
-            calculate_number_kpoints(args.sampling_density[0], 
+            calculate_number_kpoints(args.sampling_distance, 
                                      values['distance_to_hsp'])
         elif args.number_kpoints is not None:
-            calculate_sampling_distance(args.number_kpoints[0], 
+            calculate_sampling_distance(args.number_kpoints, 
                                        values['distance_to_hsp'])
     print_to_screen(values)
