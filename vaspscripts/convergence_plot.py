1fe71d63a6a709897fcec5f220e57f982598e602
lucydot <lucywhalley@gmail.com>
2017-02-16 10:11:52 +0000

    script for generating folders for phonpy jobs added. New flag to convergence_plot for reading in from a different file

--- vaspscripts/convergence_plot.py
+++ vaspscripts/convergence_plot.py
@@ -4,12 +4,12 @@ import re
 import argparse
 
 def total_energy():
-    plt, free_energies = main(begin=1,save='False', kind='electronic',file='OUTCAR')
+    plt, free_energies = main(begin=1,save='False', kind='electronic',filename='OUTCAR')
     return free_energies[-1]
 
-def main(begin=1, save=False, kind='electronic',file='OUTCAR'):
+def main(begin=1, save=False, kind='electronic',filename='OUTCAR'):
 
-    read_in = open(file,'r').read()
+    read_in = open(filename,'r').read()
 
     if kind == 'electronic':
         free_energies = re.findall("free\senergy\s*TOTEN\s*=\s*([-.\d\s]+)",read_in)     
@@ -31,8 +31,9 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--type', default='electronic', help='Select electronic or ionic convergence (default:electronic)')
     parser.add_argument('-b', '--begin', default=1, help='step to start plot at (default:1)', type=int)
     parser.add_argument('-s', '--save', default=False, help='save plot (default:False)', type=bool)
+    parser.add_argument('-f','--filename', default='OUTCAR', help='File to read from')
     args = vars(parser.parse_args())
-    free_energies = main(begin=args["begin"],save=args["save"],kind=args["type"], file="OUTCAR")
+    free_energies = main(begin=args["begin"],save=args["save"],kind=args["type"], filename=args["filename"])
     print (free_energies)
    
 
